157ee20c06fa9d9572cfaaac1746d41a5b14c667
Zari Tsu <zaritsu030907@gmail.com>
2023-04-08 10:47:43 +0800

    Update Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -7,10 +7,10 @@ public class Collatz {
     public static int nextNumber(int n) {
         if (n == 1) {
             return 1;
-        } else if (n % 2 == 0) {
-            return n / 2;
+        } else if (n % 2 == 1) {
+            return 3 * n + 1;
         } else {
-            return n * 3 + 1;
+            return n / 2;
         }
     }
 
