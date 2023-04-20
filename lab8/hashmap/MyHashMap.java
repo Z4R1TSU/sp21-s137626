dec374d8d7536c72cafd14de17983eddd08c6b13
Zari Tsu <zaritsu030907@gmail.com>
2023-04-20 19:43:53 +0800

    Update lab 8

--- lab8/hashmap/MyHashMap.java
+++ lab8/hashmap/MyHashMap.java
@@ -1,13 +1,13 @@
 package hashmap;
 
-import java.util.Collection;
+import java.util.*;
 
 /**
  *  A hash table-backed Map implementation. Provides amortized constant time
  *  access to elements via get(), remove(), and put() in the best case.
  *
  *  Assumes null keys will never be inserted, and does not resize down upon remove().
- *  @author YOUR NAME HERE
+ *  @author Zari Tsu
  */
 public class MyHashMap<K, V> implements Map61B<K, V> {
 
@@ -26,13 +26,26 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
     }
 
     /* Instance Variables */
+    private int size;
+    private int length;
+    private double loadFactor;
     private Collection<Node>[] buckets;
     // You should probably define some more!
 
     /** Constructors */
-    public MyHashMap() { }
+    public MyHashMap() {
+        this.size = 16;
+        this.length = 0;
+        this.loadFactor = 0.75;
+        this.buckets = createTable(this.size);
+    }
 
-    public MyHashMap(int initialSize) { }
+    public MyHashMap(int initialSize) {
+        this.size = initialSize;
+        this.length = 0;
+        this.loadFactor = 0.75;
+        this.buckets = createTable(this.size);
+    }
 
     /**
      * MyHashMap constructor that creates a backing array of initialSize.
@@ -41,13 +54,18 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * @param initialSize initial size of backing array
      * @param maxLoad maximum load factor
      */
-    public MyHashMap(int initialSize, double maxLoad) { }
+    public MyHashMap(int initialSize, double maxLoad) {
+        this.size = initialSize;
+        this.length = 0;
+        this.loadFactor = maxLoad;
+        this.buckets = createTable(this.size);
+    }
 
     /**
      * Returns a new node to be placed in a hash table bucket
      */
     private Node createNode(K key, V value) {
-        return null;
+        return new Node(key, value);
     }
 
     /**
@@ -69,7 +87,7 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * OWN BUCKET DATA STRUCTURES WITH THE NEW OPERATOR!
      */
     protected Collection<Node> createBucket() {
-        return null;
+        return new LinkedList<>();
     }
 
     /**
@@ -82,10 +100,137 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * @param tableSize the size of the table to create
      */
     private Collection<Node>[] createTable(int tableSize) {
+        return new Collection[tableSize];
+    }
+    // Your code won't compile until you do so!
+    @Override
+    public void clear() {
+        this.size = 16;
+        this.length = 0;
+        this.buckets = createTable(this.size);
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        return key != null && this.get(key) != null;
+    }
+
+    @Override
+    public V get(K key) {
+        if (key == null) {
+            return null;
+        }
+        int posi = getPosition(key);
+        Collection<Node> Nodes = this.buckets[posi];
+        if (Nodes == null) {
+            return null;
+        }
+        for (Node i : Nodes) {
+            if (i.key.equals(key)) {
+                return i.value;
+            }
+        }
         return null;
     }
 
-    // TODO: Implement the methods of the Map61B Interface below
-    // Your code won't compile until you do so!
+    private int getPosition(K key) {
+        int hashValue = key.hashCode();
+        return Math.floorMod(hashValue, this.size);
+    }
 
+    @Override
+    public int size() {
+        return this.length;
+    }
+
+    @Override
+    public void put(K key, V value) {
+        // Determine whether the array exceeds loadFactor, if so, reSize to double the former size.
+        if ((double) length / size > loadFactor) {
+            reSize(this.size * 2);
+        }
+
+        // Find which bucket is this key in. If the bucket does not exist, then create one.
+        int posi = getPosition(key);
+        if (this.buckets[posi] == null) {
+            this.buckets[posi] = createBucket();
+        }
+        // If the key has existed in the bucket, change the value of the map.
+        for (Node i : this.buckets[posi]) {
+            if (i.key == key) {
+                i.value = value;
+                return;
+            }
+        }
+        this.length += 1;
+        this.buckets[posi].add(createNode(key, value));
+    }
+
+    @Override
+    public Set<K> keySet() {
+        if (this.buckets == null) {
+            return null;
+        }
+        Set<K> ans = new HashSet<>();
+        for (Collection<Node> i : this.buckets) {
+            if (i != null) {
+                for (Node j : i) {
+                    ans.add(j.key);
+                }
+            }
+        }
+        return ans;
+    }
+
+    public void reSize(int s) {
+        Collection<Node>[] newBuckets = createTable(s);
+        if (this.size >= 0) System.arraycopy(this.buckets, 0, newBuckets, 0, this.size);
+        this.buckets = newBuckets;
+        this.size *= 2;
+    }
+
+    @Override
+    public V remove(K key) {
+        if (key == null || get(key) == null) {
+            return null;
+        } else {
+            V ansValue = get(key);
+            int posi = getPosition(key);
+            Collection<Node> Nodes = this.buckets[posi];
+            Nodes.removeIf(i -> i.key.equals(key));
+            return ansValue;
+        }
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        return remove(key);
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        class HashMapIter implements Iterator<K>{
+            private int bucketIndex;
+            private Collection<Node> curBucket = buckets[bucketIndex];
+            private Iterator<Node> curIter = curBucket.iterator();
+
+            HashMapIter() {
+                this.bucketIndex = 0;
+            }
+
+            public boolean hasNext() {
+                return this.bucketIndex < size() || curIter.hasNext();
+            }
+
+            public K next() {
+                if (curIter.hasNext()) {
+                    return curIter.next().key;
+                } else {
+                    this.bucketIndex += 1;
+                    return this.next();
+                }
+            }
+        }
+        return new HashMapIter();
+    }
 }
